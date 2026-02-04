c5bbf611c3a3aea06cc584b4fe9e4f600d14a366
ritik7979 <ritikchaudhary24@gmail.com>
2026-02-04 18:37:19 +0530

    11

---
+++ test.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAQI3VOQVPP3KFO4FS"
+  secret_key = "QlksyET25hlgNFVt5OAYDwy4oEZH6MuhPy0/5euE"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-055a9df0c8c9f681c"
+  instance_type = "t3.micro"
+  tags = {
+    Name = test
+  }
+}
